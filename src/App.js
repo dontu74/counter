e9268c91f677f6cd646a1e81b32ecf8bb436a55a
unknown <dongtu@gmail.com>
2020-02-05 21:45:05 -0500

    Added CSS style and methods

--- src/App.js
+++ src/App.js
@@ -7,9 +7,33 @@ class Counter extends Component {
       count: 0,
     }
 }
+
+// This where methods live
+// no need to assign variable cuz in a class
+increment = () => {
+  this.setState ({
+    count: this.state.count + 1,
+  })
+}
+
+decrement = () => {
+  this.setState ({
+    count: this.state.count - 1,
+  })
+}
+
+
   render(){
     return (
-      <div></div>
+      <div className="container">
+      <div className="navbar">Counter</div>
+      <div className="counter">
+        <h1>{this.state.count}</h1> {/*anytime the count changes, it appears here  */}
+        <button type="button" onClick={this.increment}>Increment</button>
+        <button type="button" onClick={this.decrement}>Decrement</button>
+        {/*<button type="button" onClick={this.decrement}>Restart</button> */}
+        </div>
+      </div>
     )
   }
 };
