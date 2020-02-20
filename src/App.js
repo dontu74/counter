0c32adf3ef0438c857643f3796f522bff2fb0964
unknown <dongtu@gmail.com>
2020-02-19 20:03:58 -0500

    add toggle function in double/single

--- src/App.js
+++ src/App.js
@@ -5,27 +5,57 @@ class Counter extends Component {
     super();
     this.state={
       count: 0,
+      doubleCount: false,
     }
 }
 
 // This where methods live
 // no need to assign variable cuz in a class
 increment = () => {
-  this.setState ({
-    count: this.state.count + 1,
-  })
+  if (this.state.count < 20)  {
+    this.setState ({ /*create a condition for the increment that does not pass 20 */
+      count: this.state.count + 1,
+    })
+  }
+  if (this.state.doubleCount && this.state.count < 19) {
+    this.setState({
+      count: this.state.count+2,
+    })
+  }
 }
 
 decrement = () => {
-  this.setState ({
-    count: this.state.count - 1,
-  })
+  if (this.state.count > 0) {
+    this.setState ({
+      count: this.state.count - 1,
+    })
+  }
+  if (this.state.doubleCount) {
+      if (this.state.count > 1) {
+        this.setState({
+          count: this.state.count - 2,
+        })
+      }
+    }
 }
 reset = () => {
   this.setState ({
     count: this.state.count=0,
   })
 }
+
+double = () => {
+    if (this.state.doubleCount) {
+      this.setState({
+        doubleCount: false,
+      })
+    } else {
+      this.setState({
+        doubleCount: true,
+      })
+    }
+  }
+
 
   render(){
     return (
@@ -36,6 +66,8 @@ reset = () => {
         <button type="button" onClick={this.increment}>Increment</button>
         <button type="button" onClick={this.decrement}>Decrement</button>
         <button type="button" onClick={this.reset}>Restart</button>
+        <button type="button" onClick={this.double}>{this.state.doubleCount ? 'Double Count' : 'Single Count'}</button>
+        {/*iterary statement. conditional rendering: teranry statement */}
         </div>
       </div>
     )
